9bd26ed93a73af618906c9652a1cb36b604cffed
OleksandrKotvytskyi <olexandr.kt@gmail.com>
2018-12-25 20:18:03 +0200

    Finished algorithm, modified view

--- js/HTMLCreator.js
+++ js/HTMLCreator.js
@@ -6,11 +6,14 @@ function HTMLCreator(nVariables, nRestrictions) {
 
     this.goalFuncInput = document.createElement('div');
     this.goalFuncInput.setAttribute('id', 'goalInput');
+
+    this.goalRestrictionsInput = document.createElement('div');
+    this.goalRestrictionsInput.setAttribute('id', 'restrictionsInput');
 }
 
 HTMLCreator.prototype.createAimFunctionView = function (bntStartConfirm) {
     var i, j, n,
-        inputElem, xElem, plusElem, subElem,
+        inputElem, xElem, plusElem, subElem, minElem,
         arrowElem = this.createNodeWithText('span', Text.arrow),
         body = document.body;
 
@@ -27,20 +30,113 @@ HTMLCreator.prototype.createAimFunctionView = function (bntStartConfirm) {
     for (i = 1; i <= +this.nVariables; i++) {
         inputElem = document.createElement('input');
         inputElem.setAttribute('type', 'text');
+        inputElem.setAttribute('id', i.toString());
         xElem = this.createNodeWithText('span', Text.x);
         subElem = this.createNodeWithText('sub', i);
         plusElem = this.createNodeWithText('span', "+");
+        minElem = this.createNodeWithText('span', "min");
 
         this.goalFuncInput.appendChild(inputElem);
         this.goalFuncInput.appendChild(xElem);
         this.goalFuncInput.appendChild(subElem);
 
-        i != this.nVariables ? this.goalFuncInput.appendChild(plusElem) : this.goalFuncInput.appendChild(arrowElem)
+        if (i != this.nVariables) {
+            this.goalFuncInput.appendChild(plusElem)
+        } else {
+            this.goalFuncInput.appendChild(arrowElem);
+            this.goalFuncInput.appendChild(minElem);
+        }
     }
 
     body.appendChild(this.goalFuncInput);
+    this.createRestrictionsView();
+    //console.log(math.number(math.fraction('7/22')));
+    //new Logic();
+};
 
-    new Logic();
+HTMLCreator.prototype.createRestrictionsView = function () {
+    var i, j, inputElem, xElem, plusElem, subElem, equalElem, brElem,
+        body = document.body,
+        btnStart = document.createElement('button');
+
+    btnStart.setAttribute('id', 'btnStart');
+    btnStart.innerHTML = 'Start';
+
+    for (i = 1; i <= +this.nRestrictions; i++) {
+        for (j = 1; j <= +this.nVariables; j++) {
+            inputElem = document.createElement('input');
+            inputElem.setAttribute('type', 'text');
+            inputElem.setAttribute('id', (i-1).toString() + (j-1).toString());
+            xElem = this.createNodeWithText('span', Text.x);
+            subElem = this.createNodeWithText('sub', j);
+            plusElem = this.createNodeWithText('span', "+");
+            equalElem = this.createNodeWithText('span', "=");
+
+            this.goalRestrictionsInput.appendChild(inputElem);
+            this.goalRestrictionsInput.appendChild(xElem);
+            this.goalRestrictionsInput.appendChild(subElem);
+
+            if (j != this.nVariables) {
+                this.goalRestrictionsInput.appendChild(plusElem)
+            } else {
+                this.goalRestrictionsInput.appendChild(equalElem);
+                inputElem = document.createElement('input');
+                inputElem.setAttribute('type', 'text');
+                inputElem.setAttribute('id', 'f'+i.toString());
+                this.goalRestrictionsInput.appendChild(inputElem);
+            }
+
+        }
+        brElem = document.createElement('br');
+        this.goalRestrictionsInput.appendChild(brElem);
+    }
+
+    body.appendChild(this.goalRestrictionsInput);
+    body.appendChild(btnStart);
+
+    btnStart.addEventListener('click', this.onStartClick.bind(this));
+};
+
+HTMLCreator.prototype.onStartClick = function (e) {
+    var i, j, k, childNode, fraction, logic, answer;
+        goalFunc = [], freeItems = [], matrix = [], tmpArr = [],
+        goalFuncInput = document.getElementById('goalInput'),
+        restrictionsInput = document.getElementById('restrictionsInput');
+    //this.disabled = true;
+    for (j = 0; j < this.nRestrictions; j++) {
+
+        for (k = 0; k < this.nVariables; k++) {
+            tmpArr.push(0);
+        }
+        matrix.push(tmpArr);
+        tmpArr = [];
+    }
+
+    for (i = 0; i < goalFuncInput.childNodes.length; i++) {
+        childNode = goalFuncInput.childNodes[i];
+        if (childNode.id) {
+            goalFunc.push(math.number(document.getElementById(childNode.id).value));
+        }
+    }
+
+    for (i = 0; i < restrictionsInput.childNodes.length; i++) {
+        childNode = restrictionsInput.childNodes[i];
+        if (childNode.id) {
+            //goalFunc.push(math.number(document.getElementById(childNode.id).value));
+            if (childNode.id[0] === 'f') {
+                fraction = math.fraction(document.getElementById(childNode.id).value);
+                freeItems.push(fraction.n/fraction.d * fraction.s);
+            } else {
+                //debugger;
+                fraction = math.fraction(document.getElementById(childNode.id).value);
+                matrix[childNode.id[0]][childNode.id[1]] = fraction.n/fraction.d * fraction.s;
+            }
+        }
+    }
+
+    logic = new Logic(goalFunc, matrix, freeItems);
+    answer = logic.doAlgorithm();
+    alert("Значення функції цілі : " + answer.value + " Точка: " + answer.coords.toString());
 };
 
 HTMLCreator.prototype.createNodeWithText = function(sElem, sText) {
--- js/Logic.js
+++ js/Logic.js
@@ -1,27 +1,58 @@
-function Logic() {
+function Logic(goalFunc, matrix, freeItems) {
     var i;
 
-    this.goalFunc = [6, 8, 1, 1, 0, 0];
-    this.matrix = [[-1, -2, 1, 0, 1, 0], [2, 1, 1, 1, 0, -1]];
-    //this.matrix = [[-1, 1, 1, 0, 0], [3, 0, -2, 1, 0], [1, 0, 3, 0, 1]];
-    this.freeItems = [-3, 2, 2];
+    // this.goalFunc = [-1, -1, 0, 0, 0, 0];
+    // this.matrix =[[0, 0, 1, -6, 1, 0], [0, 1, 0, 4/9, -1/9, 0], [1, 0, 0, 5/9, 1/9, 0], [0, 0, 0, -5/9, -1/9, 1]];
+    // this.freeItems = [1, 23/9, 40/9, -4/9];
+
+    this.goalFunc = goalFunc;
+    this.matrix = matrix;
+    this.freeItems = freeItems;
+
     this.basisIndexes = [];
     this.marks = [];
+    this.goalFuncValue;
+
+    this.convertedMatrix = [];
+    this.convertedFreeItems = [];
+    this.convertedMarks = [];
+    this.convertedGoalFuncValue;
 
     for (i = 0; i < this.goalFunc.length; i++) {
         this.marks[i] = 0;
     }
+}
 
+Logic.prototype.doAlgorithm = function () {
     this.findBasisIndexes();
     this.calcMarks();
+    this.findGoalFunctionMark();
     console.log('free items: ', this.checkFreeItems());
     console.log('marks: ', this.checkMarks());
-    if (this.checkFreeItems() && this.checkMarks()) {
-        this.findNewBasis();
-    } else {
-        alert("Оцінки додатні або немає від'ємних вільних членів");
+    for(;;){
+        if (!this.checkFreeItems()){
+            alert("Немає від'ємних вільних членів");
+            return this.getAnswer();
+        }
+        if (!this.checkMarks()) {
+            alert("Є додатні оцінки");
+            break;
+        }
+
+        console.log(this.findNewBasis());
+        this.doGauss();
     }
-}
+};
+
+Logic.prototype.findGoalFunctionMark = function () {
+    var i, j, mark = 0;
+
+    for (i = 0; i < this.basisIndexes.length; i++) {
+        mark += this.goalFunc[this.basisIndexes[i]] * this.freeItems[i];
+    }
+
+    this.goalFuncValue = mark;
+};
 
 Logic.prototype.calcMarks = function () {
     var i, j, n, m;
@@ -106,6 +137,7 @@ Logic.prototype.findNewBasis = function () {
     }
 
     min = arrayOfMinElems[0].value;
+    col = arrayOfMinElems[0].col;
 
     for (i = 0; i < arrayOfMinElems.length; i++) {
         if (arrayOfMinElems[i].value < min) {
@@ -114,5 +146,104 @@ Logic.prototype.findNewBasis = function () {
         }
     }
 
+    this.basisIndexes[row] = col;
+
     return {row : row, col : col};
+};
+
+Logic.prototype.doGauss = function () {
+    var i, j,
+        tmpElem,
+        basis = this.findNewBasis(),
+        row = basis.row,
+        col = basis.col,
+        basisVariable = this.matrix[row][col];
+
+    this.freeItems[row] /= basisVariable;
+
+    for (i = 0; i < this.matrix[0].length; i++) {
+        this.matrix[row][i] /= basisVariable;
+    }
+
+    for (i = 0; i < this.matrix.length; i++) {
+        if (i !== row) {
+            tmpElem = this.matrix[i][col];
+            for (j = 0; j < this.matrix[0].length; j++) {
+                this.matrix[i][j] = math.number(math.add(math.fraction(this.matrix[i][j]), math.fraction((this.matrix[row][j] * (-1)) * tmpElem)));
+            }
+
+            this.freeItems[i] = math.number(math.add(math.fraction(this.freeItems[i]), math.fraction((this.freeItems[row] * (-1)) * tmpElem)));
+        }
+    }
+
+    tmpElem = this.marks[col];
+
+    for (j = 0; j < this.marks.length; j++) {
+        this.marks[j] = math.number(math.add(math.fraction(this.marks[j]), math.fraction((this.matrix[row][j] * (-1)) * tmpElem)));
+    }
+
+    this.goalFuncValue = math.number(math.add(math.fraction(this.goalFuncValue), math.fraction((this.freeItems[row] * (-1)) * tmpElem)));
+
+    this.convertToFraction();
+
+};
+
+Logic.prototype.convertToFraction = function () {
+    var i, j, fraction, fractionString,
+        tmpArray = [];
+
+    function getFraction(array) {
+        var tmpArray = [];
+
+        for (j = 0; j < array.length; j++) {
+            fraction = math.fraction(array[j]);
+
+            if (fraction.n === 0) {
+                fractionString = "0";
+            } else if (fraction.d === 1) {
+                fractionString = fraction.n.toString();
+            } else {
+                fractionString = fraction.n.toString() + "/" + fraction.d.toString();
+            }
+
+            (fraction.s === -1) ? tmpArray.push("-" + fractionString) : tmpArray.push(fractionString);
+        }
+
+        return tmpArray;
+    }
+
+    for (i = 0; i < this.matrix.length; i++) {
+        this.convertedMatrix[i] = getFraction(this.matrix[i]);
+    }
+
+    this.convertedFreeItems = getFraction(this.freeItems);
+    this.convertedMarks = getFraction(this.marks);
+    this.convertedGoalFuncValue = getFraction([this.goalFuncValue])[0];
+
+    console.log(this.convertedMatrix, this.convertedFreeItems, this.convertedMarks, this.convertedGoalFuncValue);
+    this.convertedMatrix = [];
+};
+
+Logic.prototype.getAnswer = function () {
+    var i, j,
+        answer = {},
+        goalFuncAnswerIndexes = [];
+    answer.coords = [];
+    for (i = 0; i < this.goalFunc.length; i++) {
+        if (this.goalFunc[i] !== 0) {
+            goalFuncAnswerIndexes.push(i);
+        }
+    }
+
+    for (i = 0; i < this.basisIndexes.length; i++) {
+        for (j = 0; j < goalFuncAnswerIndexes.length; j++) {
+            if (this.basisIndexes[i] === goalFuncAnswerIndexes[j]) {
+                answer.coords.push(this.convertedFreeItems[i]);
+            }
+        }
+    }
+
+    answer.value = this.convertedGoalFuncValue;
+
+    return answer;
 };
